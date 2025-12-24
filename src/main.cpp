fb867313e60b4baab3bdbf061bb41af593b56791
Krithik Senthilkumar <krithikcloud@gmail.com>
2025-12-24 16:15:32 -0600

    corrected drivetrain directions - 4 motors one way, 2 back motors opposite

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,8 @@
 // Chassis Configuration
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {-19, 18, 17},   // Left Chassis Ports (Left back 19 reversed from middle/front)
-    {16, -15, -14},  // Right Chassis Ports (Right back 16 reversed from middle/front)
+    {19, -18, -17},  // Left Chassis Ports (back 19 normal, middle 18 & front 17 reversed)
+    {-16, 15, 14},   // Right Chassis Ports (back 16 reversed, middle 15 & front 14 normal)
 
     10,     // IMU Port (Vex key 10)
     4.125,  // Wheel diameter
