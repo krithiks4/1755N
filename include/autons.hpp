f7f61ffb06bffe05bc8728a23dbd7f24e3e35bf5
Ohno17 <ajaylego30@gmail.com>
2026-01-02 16:09:03 -0600

    oops header

--- include/autons.hpp
+++ include/autons.hpp
@@ -11,6 +11,7 @@ void default_constants();
 void right_side_auton(); // Right side autonomous routine
 void left_side_auton(); // Left side autonomous routine
 
+void skills_half();
 void skills_auton();
 
 #pragma region tests
