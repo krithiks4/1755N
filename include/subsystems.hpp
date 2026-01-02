d8b9cc3d9a9fe8dd83cfe3385b730402355b6fa8
Krithik Senthilkumar <krithikcloud@gmail.com>
2026-01-02 12:28:52 -0600

    fixed little will mech falling on default

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -8,5 +8,5 @@ extern Scoring scoring;
 
 // Pneumatics
 // Middle goal piston (H) and Tongue mech piston (G)
-inline ez::Piston middle_goal_piston('H', true);  // Middle goal piston
+inline ez::Piston middle_goal_piston('H', false);  // Middle goal piston
 inline ez::Piston tongue_piston('G', true);  // Tongue mech piston
