25775118384a10ee69164f8d34265812be349d78
Krithik Senthilkumar <krithikcloud@gmail.com>
2026-01-02 12:29:36 -0600

    random slash gone

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -9,6 +9,6 @@ extern Intake intake;
 // ===== PNEUMATICS =====
 // Four individual pistons
 inline ez::Piston intake_lift('E', false); 
-inline ez::Piston indexer('F', false);     \
+inline ez::Piston indexer('F', false);     
 inline ez::Piston wing('G', false);        
 inline ez::Piston little_will_mech('H', true);  
