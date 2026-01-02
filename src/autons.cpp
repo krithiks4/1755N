cdeedbb984310ede9d1386019ed5b98481980f1b
Krithik Senthilkumar <krithikcloud@gmail.com>
2026-01-02 13:06:41 -0600

    turn pid completed

--- src/autons.cpp
+++ src/autons.cpp
@@ -12,7 +12,7 @@ void default_constants() {
   // P, I, D, and Start I
   chassis.pid_drive_constants_set(5.0, 0.0, 63.0);         // Fwd/rev constants
   chassis.pid_heading_constants_set(0.0, 0.0, 0.0);        // DISABLED heading correction to prevent spinning
-  chassis.pid_turn_constants_set(1.8, 0.0, 25.0, 15);      // Turn in place constants
+  chassis.pid_turn_constants_set(2.5, 0.0, 25.0, 15);      // Turn in place constants
   chassis.pid_swing_constants_set(5.0, 0.0, 30.0);         // Swing constants
   chassis.pid_odom_angular_constants_set(6.0, 0.0, 50.0);  // Angular control for odom motions
   chassis.pid_odom_boomerang_constants_set(5.0, 0.0, 45.0); // Angular control for boomerang motions
